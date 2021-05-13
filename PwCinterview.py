f1a93899077da36a6f4235d0004a1e93d6eba207
Jaibean <jaimie.bertoli@gmail.com>
2021-05-12 22:22:57 -0400

    pwc_interview
    sql & python

--- PwCinterview.py
+++ PwCinterview.py
@@ -1,4 +1,33 @@
 
+SQL
+
+Staff Table with their supervisor details. Find all the staff who are earning more than their supervisor.
+
+Table: Staff
+
++----+-------+--------+-----------+
+| Id | Name    | Salary | SupervisorId |
++----+-------+--------+---------------+
+| 1  | Jack    | 70000  | 3           |
+| 2  | William | 80000  | 4           |
+| 3  | Samuel  | 60000  | NULL        |
+| 4  | Michael | 90000  | NULL        |
++----+-------+--------+---------------+
+
+SELECT FROM TABLE Staff, Name, Salary, SupervisorID
+INNER JOIN Staff ON Id
+
+
+
+select s1.name from
+staff s1 join staff s2 on s1.superv
+
+
+
+
+
+
+
 """Python:
 
 List of words are provided, so club all the words formed with  same characters (anagrams) and return it as a list
--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,8 @@
 
 #fizzbuzz
 
+
+
 numbers = [45, 22, 14, 65, 97, 72]
 for i, num in enumerate(numbers):
     if num % 3 == 0 and num % 5 == 0:
@@ -10,4 +12,4 @@ for i, num in enumerate(numbers):
     elif num % 5 == 0:
         numbers[i] = 'buzz'
 
-console.log(numbers)
+print(numbers)
