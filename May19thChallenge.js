b8c26d973a23fd87801200f3d104bbde1e14f009
Jaibean <jaimie.bertoli@gmail.com>
2021-05-25 22:40:47 -0400

    may19thchallenge

---
+++ May19thChallenge.js
@@ -0,0 +1,14 @@
+// Your goal is to write a function that will receive an array of strings as its argument and returns an array of strings each representing the longest substring
+// of contiguous vowels  ( aeiouAEIOU ).
+
+
+str1 = "what a beautiful day today"
+str2 = "it's okay, but very breezy"
+strArray = [str1,str2]
+
+contiguousVowels(strArray) {
+
+}  
+
+// Output to window here:
+document.getElementById("result-box").innerHTML =
