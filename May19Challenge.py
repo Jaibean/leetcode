441a6c1ed939ad84d9184be19df54b44670d2aa9
Jaibean <jaimie.bertoli@gmail.com>
2021-05-27 14:14:46 -0400

    code update

---
+++ May19Challenge.py
@@ -0,0 +1,15 @@
+"""Your goal is to write a function that will receive an array of strings as its argument and returns an array of strings each representing the longest substring
+of contiguous vowels  ( aeiouAEIOU ). """
+
+
+str1 = "what a beautiful day today"
+str2 = "it's okay, but very breezy"
+strArray = [str1,str2]
+
+contiguousVowels(strArray) {
+for count, value in enumerate(strArray):
+    if 
+}  
+
+// Output to window here:
+document.getElementById("result-box").innerHTML =
--- May19thChallenge.js
+++ May19thChallenge.js
@@ -6,9 +6,21 @@ str1 = "what a beautiful day today"
 str2 = "it's okay, but very breezy"
 strArray = [str1,str2]
 
-contiguousVowels(strArray) {
+const vowels = ["a", "e", "i", "o", "u"]
 
+contiguousVowels(strArray) {
+    const vowels = /[aeiouAEIOU]+/g;  // g means global 
+    for (let i in strArray){
+        const letters = strArray[i].match(vowels);
+            if (letters) {
+                const longest = letters.reduce((acc, val)) => acc.length > val.length ? acc : val, '');
+                result.push(longest);
+            }
+    }
+    return result;
 }  
 
+contigousVowels(strArray)
+
 // Output to window here:
-document.getElementById("result-box").innerHTML =
+document.getElementById("result-box").innerHTML = `[${contigousVowels(strArray)}]`
