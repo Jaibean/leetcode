61a3b735551a72bacf89902ff3e488296ac7b12c
Jaimie Bertoli <jaimie.bertoli@gmail.com>
2021-11-15 12:46:07 -0500

    isPalindrom with regex

--- js/ArrayandStrings/isPalindrome.js
+++ js/ArrayandStrings/isPalindrome.js
@@ -1,12 +1,33 @@
 // is plaindrome
 
-const clean = str => str.toLowerCase('').split(' ').join('')
+//with regex
 const isPalindrom = str => {
-    let cleanStr = clean(str)
-    let reverseStr = cleanStr.split(' ').reverse('').join('');
+    let reg = /[\W_]/g;
+    let cleanStr = str.toLowerCase('').replace(reg, '')
+    let reverseStr = cleanStr.split('').reverse('').join('');
     return str === reverseStr;
 
 }
+
+
+
+// const isPalindrom = str => {
+//     let len = str.length;
+//     let start = str.substring(0, Math.floor(len/2)).toLowerCase();
+//     let end = str.substring(len - Math.floor(len/2)).toLowerCase();
+//     let flip = end.split('').reverse().join('');
+//     return start === flip
+
+// };
+
+
+// const clean = str => str.toLowerCase('').split(' ').join('')
+// const isPalindrom = str => {
+//     let cleanStr = clean(str)
+//     let reverseStr = cleanStr.split(' ').reverse('').join('');
+//     return str === reverseStr;
+
+// }
 
 
 console.log(isPalindrom('race car'));
