52b37848221fc2fdf40115a08c5c76935e17507c
Jaimie Bertoli <jaimie.bertoli@gmail.com>
2021-11-15 12:47:33 -0500

    stepping through solutions

--- js/ArrayandStrings/compressString.js
+++ js/ArrayandStrings/compressString.js
@@ -7,24 +7,29 @@
 // You can assume that the input only contains alphabetic characters.
 
 const compress = (s) => {
-  let result = [];
-  let i = 0;
-  let j = 0;
-
-  while (j <= s.length) {
-    if (s[i] === s[j]) {
-      j += 1;
-    } else {
-      const num = j - i;
-      if (num === 1) {
-        result.push(s[i]);
+    let result = [];
+    //start of consecutive streak
+    let i = 0;
+    //end of consecutive streak
+    let j = 0;
+    
+    // less than equal to in order to use the index of the last empty character
+    while (j <= s.length) {
+      //looking for the streak to stop
+      if (s[i] === s[j]) {
+        j++
       } else {
-        result.push(String(num), s[i]);
+        // var for the amount the streak lasts
+        const num = j - i;
+        // if there is only one streak, no number is needed
+        if (num === 1 ) {
+          result.push(s[i])
+        } else {
+          // if there is more than one streak
+          result.push(String(num), s[i]);
+        }
+        i = j;
       }
-      i = j;
     }
-  }
-  return result.join('');
-};
-
-console.log(compress("ccsstssss"));
+    return result.join('');
+  };
--- js/ArrayandStrings/uncompressString.js
+++ js/ArrayandStrings/uncompressString.js
@@ -4,26 +4,37 @@
 // The function should return an uncompressed version of the string where each 'char' of a group is repeated 'number' times consecutively. 
 // You may assume that the input string is well-formed according to the previously mentioned pattern.
 
-const uncompress = (s) => {
-    let output = [];
-    const nums = '0123456789'
+const uncompress = s => {
+    // pointers both starting at index 0 to count number portion of string
     let i = 0;
+    // end of number portion
     let j = 0;
-    while (j < s.length) {
-      if(nums.includes(s[j])) {
-        j += 1;
+    // reference string for numbers
+    let nums = '0123456789'
+    // outout array for finall result
+    let output = [];
+    // loop through length of string using j as pointer
+    while (j < s.length){
+      //looking to see if the string of numbers include string at j index
+      if (nums.includes(s[j])){
+        // if it does then progress j to the next index
+        j++
+        // once j is nont a number we have to take those numbers behind it
       } else {
-        num = Number(s.slice(i, j))
+        // use Number cast to get number between i and j but not including j (slice)
+        let num = Number(s.slice(i,j))
+        //loop through as many times that number apears to push the j character to output
         for (let count = 0; count < num; count++){
+          //push character to end of array for constant time
           output.push(s[j]);
         }
+        // keep progressing j
         j++;
+        //bring i up to speed
         i = j;
       }
-      
     }
-    
-      return output.join('');
+    // need to join to make the array a string of all characters
+    return output.join('')
   };
-  
   
